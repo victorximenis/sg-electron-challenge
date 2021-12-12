d512fd9bc644c48f632c3fa569843bbfcb9de527
Clênio Galvão <clenio.cgm@gmail.com>
2021-12-12 08:30:28 -0300

    First version with no js

--- renderer.js
+++ renderer.js
@@ -4,3 +4,5 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+//https://api.openweathermap.org/data/2.5/weather?q=london&appid=80f895757aaaa1e4a1fbe36d82a9880e
